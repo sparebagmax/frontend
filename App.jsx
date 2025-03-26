99c6a8282a23d212066166b5f95ad6e4153f76b1
sparebagmax <sparebagmax@gmail.com>
2025-03-27 00:35:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import RES from './components/Registration/registration';
+import LOG from './components/LoginPage/login';
+import LAND from './components/LandingPage/landingpage';
+import ADDLIST from './components/AddListing/addlisting'
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <ADDLIST/>
+      {/* <LAND/>
+      <RES/>
+      <LOG/> */}
+    </div>
+  );
+}
+
+export default App;
